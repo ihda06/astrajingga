e3cb2bab250687e13b5433a4c31fe0fdf6038101
ihda <ihda_anwar@ajari.live>
2025-11-02 13:29:50 +0700

    feat: implement dynamic import for TabsSelector with loading placeholder in MainHeader

--- src/app/blog/_components/MainHeader.tsx
+++ src/app/blog/_components/MainHeader.tsx
@@ -1,4 +1,21 @@
-import TabsSelector from "./TabsSelector";
+import dynamic from "next/dynamic";
+
+const TabsSelector = dynamic(() => import("./TabsSelector"), {
+  loading: () => (
+    <div className="flex ">
+      <div className="flex gap-2 items-center justify-center px-2 py-2 border-b border-gray-200 bg-gray-700 rounded-full">
+        {[1, 2].map((index) => (
+          <div
+            key={index}
+            className="px-4 py-1 rounded-full bg-gray-600 animate-pulse"
+          >
+            <div className="h-4 w-16 bg-gray-500 rounded"></div>
+          </div>
+        ))}
+      </div>
+    </div>
+  ),
+});
 
 export default function MainHeader() {
   return (
