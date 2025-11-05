8b00d648085e2dea675afb19e6b5749e58a4d1f9
ihda <ihdaanwari5@gmail.com>
2025-11-05 10:47:48 +0700

    refactor: adjust blog card

--- src/app/blog/_components/BlogCard.tsx
+++ src/app/blog/_components/BlogCard.tsx
@@ -13,30 +13,34 @@ export default function BlogCard({
   public_reactions_count,
 }: Blog) {
   return (
-    <div className="w-full bg-white space-y-3 p-4 border rounded-lg">
+    <div className="w-full bg-white space-y-3 p-4 border h-full flex flex-col rounded-lg">
       <div className="w-full h-48 flex items-center justify-center overflow-hidden">
         <Image src={cover_image} alt={title} width={500} height={500} />
       </div>
 
-      <div className="flex flex-col gap-2">
-        <h2 className="text-xl font-bold">{title}</h2>
-        <p className="text-sm text-gray-500">{description}</p>
-      </div>
-      <div className="flex justify-between items-center">
-        <p className="text-xs text-gray-500">
-          {reading_time_minutes} minutes read |{" "}
-          {dayjs(created_at).format("DD/MM/YYYY")}
-        </p>
-        <div className="flex items-center gap-3">
-          <div className="flex items-center gap-1">
-            <FaComments className="text-gray-500" size={10} />
-            <span className="text-xs text-gray-500">{comments_count}</span>
+      <div className="flex flex-col flex-1 justify-between">
+        <div className="flex flex-col gap-2">
+          <h2 className="text-xl font-bold">{title}</h2>
+          <div className="flex-1">
+            <p className="text-sm text-gray-500">{description}</p>
           </div>
-          <div className="flex items-center gap-1">
-            <FaHeart className="text-red-500" size={10} />
-            <span className="text-xs text-gray-500">
-              {public_reactions_count}
-            </span>
+        </div>
+        <div className="flex justify-between items-center">
+          <p className="text-xs text-gray-500">
+            {reading_time_minutes} minutes read |{" "}
+            {dayjs(created_at).format("DD/MM/YYYY")}
+          </p>
+          <div className="flex items-center gap-3">
+            <div className="flex items-center gap-1">
+              <FaComments className="text-gray-500" size={10} />
+              <span className="text-xs text-gray-500">{comments_count}</span>
+            </div>
+            <div className="flex items-center gap-1">
+              <FaHeart className="text-red-500" size={10} />
+              <span className="text-xs text-gray-500">
+                {public_reactions_count}
+              </span>
+            </div>
           </div>
         </div>
       </div>
